deed554b7b559c5b291d401d9575e2532c35b124
Roger <2017141043@unh.edu.pe>
2021-10-05 15:30:28 -0500

    data persintent

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const Binance = require("./utils/fech");
-const { minPrice } = require("./utils/logic");
+const { minPrice, ggg } = require("./utils/logic");
 
 const main = async () => {
   const binance = new Binance();
   const result = await binance.getDataBinance();
-  //console.log(result);
+
   const priceOptimum = minPrice(result);
   console.log(priceOptimum);
+  console.log(ggg());
 };
 
 main();
--- utils/logic.js
+++ utils/logic.js
@@ -1,13 +1,55 @@
+const fs = require("fs");
 /* 
-choose the lowest price
+1. choose the lowest price
+2. add to list
 */
+
+let globalPrice = [];
+const path = "./db/data.json";
+
 const minPrice = (prices = []) => {
   let priceNumber = prices.map((i) => Number(i));
   let min = Math.min(...priceNumber);
+  addHistoryPrice(min);
+
+  //globalPrice.push(min);
+
   console.log(priceNumber);
+  // console.log(globalPrice);
   return min;
+};
+
+const addHistoryPrice = (price) => {
+  if (globalPrice.includes(price)) {
+    return;
+  }
+  globalPrice = globalPrice.slice(0, 5);
+  globalPrice.unshift(price);
+
+  saveDB();
+};
+
+const saveDB = () => {
+  const payload = {
+    prices: globalPrice,
+  };
+  fs.writeFileSync(path, JSON.stringify(payload));
+};
+
+const readDB = (() => {
+  if (!fs.existsSync(path)) {
+    return;
+  }
+  const info = fs.readFileSync(path, "utf8");
+  const data = JSON.parse(info);
+  globalPrice = data.prices;
+})();
+
+const ggg = () => {
+  return globalPrice;
 };
 
 module.exports = {
   minPrice,
+  ggg,
 };
